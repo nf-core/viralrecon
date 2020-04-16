82252cbc590e70994e6fafe8bac24b0a2f526514
drpatelh <drhpatel@gmail.com>
2020-04-16 15:35:55 +0100

    Minor updates

--- bin/ivar_variants_to_vcf.py
+++ bin/ivar_variants_to_vcf.py
@@ -13,26 +13,25 @@ def parse_args(args=None):
     parser.add_argument('FILE_OUT', help="Full path to output vcf file")
     return parser.parse_args(args)
 
-def parse_tsv(FileIn,OutPrefix):
-    file_basename=os.path.splitext(FileIn)[0]
-    header_1="""##fileformat=VCFv4.2
-##source=iVar
-##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
-##FILTER=<ID=PASS,Description="Result of p-value <= 0.05">
-##FILTER=<ID=FAIL,Description="Result of p-value > 0.05">
-##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
-##FORMAT=<ID=REF_DP,Number=1,Type=Integer,Description="Depth of reference base">
-##FORMAT=<ID=REF_RV,Number=1,Type=Integer,Description="Depth of reference base on reverse reads">
-##FORMAT=<ID=REF_QUAL,Number=1,Type=Integer,Description="Mean quality of reference base">
-##FORMAT=<ID=ALT_DP,Number=1,Type=Integer,Description="Depth of alternate base">
-##FORMAT=<ID=ALT_RV,Number=1,Type=Integer,Description="Deapth of alternate base on reverse reads">
-##FORMAT=<ID=ALT_QUAL,Number=1,Type=String,Description="Mean quality of alternate base">
-##FORMAT=<ID=ALT_FREQ,Number=1,Type=String,Description="Frequency of alternate base">\n"""
-    header_2='#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t'+file_basename+'\n'
+def ivar_variants_to_vcf(FileIn,FileOut):
+    filename = os.path.splitext(FileIn)[0]
+    header = """##fileformat=VCFv4.2
+                ##source=iVar
+                ##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
+                ##FILTER=<ID=PASS,Description="Result of p-value <= 0.05">
+                ##FILTER=<ID=FAIL,Description="Result of p-value > 0.05">
+                ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+                ##FORMAT=<ID=REF_DP,Number=1,Type=Integer,Description="Depth of reference base">
+                ##FORMAT=<ID=REF_RV,Number=1,Type=Integer,Description="Depth of reference base on reverse reads">
+                ##FORMAT=<ID=REF_QUAL,Number=1,Type=Integer,Description="Mean quality of reference base">
+                ##FORMAT=<ID=ALT_DP,Number=1,Type=Integer,Description="Depth of alternate base">
+                ##FORMAT=<ID=ALT_RV,Number=1,Type=Integer,Description="Deapth of alternate base on reverse reads">
+                ##FORMAT=<ID=ALT_QUAL,Number=1,Type=String,Description="Mean quality of alternate base">
+                ##FORMAT=<ID=ALT_FREQ,Number=1,Type=String,Description="Frequency of alternate base">\n"""
+    header += '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t'+filename+'\n'
 
-    o = open(OutPrefix,'w')
-    o.write(header_1)
-    o.write(header_2)
+    fout = open(FileOut,'w')
+    fout.write(header)
     with open(FileIn) as f:
         for line in f:
             if not re.match("REGION",line):
@@ -52,12 +51,13 @@ def parse_tsv(FileIn,OutPrefix):
                 FORMAT='GT:REF_DP:REF_RV:REF_QUAL:ALT_DP:ALT_RV:ALT_QUAL:ALT_FREQ'
                 SAMPLE='1:'+line[4]+':'+line[5]+':'+line[6]+':'+line[7]+':'+line[8]+':'+line[9]+':'+line[10]
                 line = CHROM+'\t'+POS+'\t'+ID+'\t'+REF+'\t'+ALT+'\t'+QUAL+'\t'+FILTER+'\t'+INFO+'\t'+FORMAT+'\t'+SAMPLE+'\n'
-                o.write(line)
-    o.close()
+                fout.write(line)
+    fout.close()
+
 
 def main(args=None):
     args = parse_args(args)
-    parse_tsv(args.FILE_IN,args.FILE_OUT)
+    ivar_variants_to_vcf(args.FILE_IN,args.FILE_OUT)
 
 
 if __name__ == '__main__':
