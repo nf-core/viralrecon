15d2fd5f4e41577cfab81b8f66ff3cc23863f183
svarona <sarai.var.fer@gmail.com>
2020-04-16 15:59:40 +0200

    Changed suggestions

--- bin/ivar_variants_to_vcf.py
+++ bin/ivar_variants_to_vcf.py
@@ -5,17 +5,29 @@ import re
 import argparse
 
 def parse_args(args=None):
-    Description = 'Reformat iVar tsv files to vcf files.'
-    Epilog = """Example usage: python tsv_to_vcf.py <FILE_IN> <FILE_OUT>"""
+    Description = 'Convert iVar variants tsv file to vcf format.'
+    Epilog = """Example usage: python ivar_variants_to_vcf.py <FILE_IN> <FILE_OUT>"""
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument('FILE_IN', help="Input tsv file.")
-    parser.add_argument('FILE_OUT', help="Output file with .vfc.")
+    parser.add_argument('FILE_OUT', help="Full path to output vcf file")
     return parser.parse_args(args)
 
 def parse_tsv(FileIn,OutPrefix):
     file_basename=os.path.splitext(FileIn)[0]
-    header_1='##fileformat=VCFv4.2\n##source=iVar\n##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">\n##FILTER=<ID=PASS,Description="Result of p-value <= 0.05">\n##FILTER=<ID=nopass,Description="Result of p-value > 0.05">\n##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n##FORMAT=<ID=REF_DP,Number=1,Type=Integer,Description="Depth of reference base">\n##FORMAT=<ID=REF_RV,Number=1,Type=Integer,Description="Depth of reference base on reverse reads">\n##FORMAT=<ID=REF_QUAL,Number=1,Type=Integer,Description="Mean quality of reference base">\n##FORMAT=<ID=ALT_DP,Number=1,Type=Integer,Description="Depth of alternate base">\n##FORMAT=<ID=ALT_RV,Number=1,Type=Integer,Description="Deapth of alternate base on reverse reads">\n##FORMAT=<ID=ALT_QUAL,Number=1,Type=String,Description="Mean quality of alternate base">\n##FORMAT=<ID=ALT_FREQ,Number=1,Type=String,Description="Frequency of alternate base">\n'
+    header_1="""##fileformat=VCFv4.2
+##source=iVar
+##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
+##FILTER=<ID=PASS,Description="Result of p-value <= 0.05">
+##FILTER=<ID=FAIL,Description="Result of p-value > 0.05">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=REF_DP,Number=1,Type=Integer,Description="Depth of reference base">
+##FORMAT=<ID=REF_RV,Number=1,Type=Integer,Description="Depth of reference base on reverse reads">
+##FORMAT=<ID=REF_QUAL,Number=1,Type=Integer,Description="Mean quality of reference base">
+##FORMAT=<ID=ALT_DP,Number=1,Type=Integer,Description="Depth of alternate base">
+##FORMAT=<ID=ALT_RV,Number=1,Type=Integer,Description="Deapth of alternate base on reverse reads">
+##FORMAT=<ID=ALT_QUAL,Number=1,Type=String,Description="Mean quality of alternate base">
+##FORMAT=<ID=ALT_FREQ,Number=1,Type=String,Description="Frequency of alternate base">\n"""
     header_2='#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t'+file_basename+'\n'
 
     o = open(OutPrefix,'w')
@@ -35,7 +47,7 @@ def parse_tsv(FileIn,OutPrefix):
                 if pass_test == 'TRUE':
                     FILTER='PASS'
                 else:
-                    FILTER='nopass'
+                    FILTER='FAIL'
                 INFO='DP='+line[11]
                 FORMAT='GT:REF_DP:REF_RV:REF_QUAL:ALT_DP:ALT_RV:ALT_QUAL:ALT_FREQ'
                 SAMPLE='1:'+line[4]+':'+line[5]+':'+line[6]+':'+line[7]+':'+line[8]+':'+line[9]+':'+line[10]
