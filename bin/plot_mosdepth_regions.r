3104925086a98f8a20da652c11066293420eacd4
drpatelh <drhpatel@gmail.com>
2020-06-16 17:26:38 +0100

    Update for multiple samples

--- bin/plot_mosdepth_regions.r
+++ bin/plot_mosdepth_regions.r
@@ -9,66 +9,154 @@
 library(optparse)
 library(ggplot2)
 library(scales)
+library(ComplexHeatmap)
+library(viridis)
+library(tidyverse)
 
 ################################################
 ################################################
-## PARSE COMMAND-LINE PARAMETERS              ##
+## VALIDATE COMMAND-LINE PARAMETERS           ##
 ################################################
 ################################################
 
-option_list <- list(make_option(c("-i", "--region_file"), type="character", default=NULL, help="mosdepth regions output file (typically ends with *.regions.bed.gz)", metavar="path"),
-                    make_option(c("-s", "--sample_name"), type="character", default=NULL, help="Sample name for plot title. If not provided will be extracted from --region_file", metavar="string"),
-                    make_option(c("-o", "--out_file"), type="character", default=NULL, help="Full path to pdf output file. If not provide will be extracted from --region_file", metavar="path"))
+option_list <- list(make_option(c("-i", "--input_files"), type="character", default=NULL, help="Comma-separated list of mosdepth regions output file (typically end in *.regions.bed.gz)", metavar="input_files"),
+                    make_option(c("-s", "--input_suffix"), type="character", default='.regions.bed.gz', help="Portion of filename after sample name to trim for plot title e.g. '.regions.bed.gz' if 'SAMPLE1.regions.bed.gz'", metavar="input_suffix"),
+                    make_option(c("-o", "--output_dir"), type="character", default='./', help="Output directory", metavar="path"),
+                    make_option(c("-p", "--output_suffix"), type="character", default='regions', help="Output suffix", metavar="output_suffix"))
 
 opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
-if (is.null(opt$region_file)){
+## Check input files
+INPUT_FILES <- unique(unlist(strsplit(opt$input_files,",")))
+if (length(INPUT_FILES) == 0) {
     print_help(opt_parser)
-    stop("At least one mosdepth region file must be supplied", call.=FALSE)
+    stop("At least one input file must be supplied", call.=FALSE)
+}
+if (!all(file.exists(INPUT_FILES))) {
+    stop(paste("The following input files don't exist:",paste(INPUT_FILES[!file.exists(INPUT_FILES)], sep='', collapse=' '), sep=' '), call.=FALSE)
 }
 
-SAMPLE_NAME = opt$sample_name
-if (is.null(opt$sample_name)){
-    SAMPLE_NAME = gsub('.regions.bed.gz','',basename(opt$region_file))
+## Check the output directory has a trailing slash, if not add one
+OUTDIR <- opt$output_dir
+if (tail(strsplit(OUTDIR,"")[[1]],1)!="/") {
+    OUTDIR <- paste(OUTDIR,"/",sep='')
+}
+## Create the directory if it doesn't already exist.
+if (!file.exists(OUTDIR)) {
+  dir.create(OUTDIR,recursive=TRUE)
 }
 
-OUT_FILE = opt$out_file
-if (is.null(opt$out_file)){
-    OUT_FILE = gsub('.gz','.pdf',opt$region_file)
-}
-if (file.exists(dirname(OUT_FILE)) == FALSE) {
-    dir.create(dirname(OUT_FILE),recursive=TRUE)
-}
+#bin/plot_mosdepth_regions.r -i /camp/stp/babs/working/patelh/code/nextflow/nfcore/viralrecon/results/variants/bam/mosdepth/amplicon/SAMPLE1_PE.trim.mkD.amplicon.regions.bed.gz,/camp/stp/babs/working/patelh/code/nextflow/nfcore/viralrecon/results/variants/bam/mosdepth/amplicon/SAMPLE3_SE.trim.mkD.amplicon.regions.bed.gz -s .trim.mkD.amplicon.regions.bed.gz -o ./test
 
 ################################################
 ################################################
-## PLOT COVERAGE                              ##
+## READ IN DATA                               ##
 ################################################
 ################################################
 
 ## Read in data
-dat <- read.csv(gzfile(opt$region_file,'r'),sep="\t", header=FALSE)
-colnames(dat) <- c('chrom', 'start','end', 'coverage')
-dat$coverage <- dat$coverage + 1
+dat <- NULL
+for (input_file in INPUT_FILES) {
+    sample = gsub(opt$input_suffix,'',basename(input_file))
+    dat <- rbind(dat, cbind(read.delim(input_file, header=FALSE, sep='\t', stringsAsFactors=FALSE, check.names=FALSE)[,-6], sample, stringsAsFactors=F))
+}
 
-## Coverage plot
-plot <- ggplot(dat,aes(x=end,y=coverage)) +
-        geom_ribbon(aes(ymin = 0, ymax = coverage), data =) +
-        theme_bw() +
-        scale_x_continuous(expand = c(0, 0)) +
-        scale_y_continuous(trans = log10_trans(),
-                           breaks = trans_breaks("log10", function(x) 10^x),
-                           labels = trans_format("log10", math_format(10^.x)),
-                           expand = c(0, 0)) +
-        ylab(bquote('log'[10]~'(Coverage+1)')) +
-        xlab("Position (bp)") +
-        ggtitle(paste(SAMPLE_NAME,"coverage"))
+## Reformat table
+if (ncol(dat) == 6) {
+    colnames(dat) <- c('chrom', 'start','end', 'region', 'coverage', 'sample')
+    dat$region <- factor(dat$region, levels=unique(dat$region[order(dat$start, decreasing=TRUE)]))
+} else {
+    colnames(dat) <- c('chrom', 'start','end', 'coverage', 'sample')
+}
+dat$sample <- factor(dat$sample, levels=sort(unique(dat$sample)))
 
-## Export plot to file
-pdf(file=OUT_FILE,height=6,width=12)
-print(plot)
-dev.off()
+## Write merged coverage data for all samples to file
+outfile <- paste(OUTDIR,"all_samples.",opt$output_suffix,".coverage.tsv", sep='')
+write.table(dat, file=outfile, col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
+
+################################################
+################################################
+## PER-SAMPLE COVERAGE PLOTS                  ##
+################################################
+################################################
+
+for (sample in unique(dat$sample)) {
+    sample_dat <- dat[dat$sample == sample,]
+    outfile <- paste(OUTDIR,sample,".",opt$output_suffix,".coverage.tsv", sep='')
+    write.table(sample_dat,file=outfile, col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
+    sample_dat$coverage <- sample_dat$coverage + 1
+
+    if (ncol(sample_dat) == 6) {
+        plot <- ggplot(sample_dat,aes(x=region,y=coverage)) +
+                geom_bar(stat="identity", fill="#D55E00", width=0.6) +
+                theme_bw() +
+                theme(plot.title=element_text(size=10),
+                      axis.text.x=element_text(size=10),
+                      axis.text.y=element_text(size=8)) +
+                coord_flip() +
+                scale_x_discrete(expand=c(0, 0)) +
+                scale_y_continuous(trans=log10_trans(),
+                                   breaks=trans_breaks('log10', function(x) 10^x),
+                                   labels=trans_format('log10', math_format(10^.x)),
+                                   expand=c(0, 0)) +
+                ylab(bquote('log'[10]~'(Coverage+1)')) +
+                xlab('Amplicon') +
+                ggtitle(paste(sample,'per amplicon coverage'))
+
+          outfile <- paste(OUTDIR,sample,".",opt$output_suffix,".coverage.pdf", sep='')
+          ggsave(file=outfile, plot, width=16, height=3+(0.3*length(unique(sample_dat$region))), units="cm")
+    } else {
+        plot <- ggplot(sample_dat,aes(x=end,y=coverage)) +
+                geom_ribbon(aes(ymin=0, ymax=coverage), fill="#D55E00", data=) +
+                theme_bw() +
+                scale_x_continuous(expand=c(0, 0)) +
+                scale_y_continuous(trans=log10_trans(),
+                                   breaks=trans_breaks('log10', function(x) 10^x),
+                                   labels=trans_format('log10', math_format(10^.x)),
+                                   expand=c(0, 0)) +
+                ylab(bquote('log'[10]~'(Coverage+1)')) +
+                xlab('Position (bp)') +
+                ggtitle(paste(SAMPLE_NAME,'coverage'))
+
+        outfile <- paste(OUTDIR,sample,".",opt$output_suffix,".coverage.pdf", sep='')
+        ggsave(file=outfile, plot, width=12, height=6, units="cm")
+    }
+}
+
+################################################
+################################################
+## REGION-BASED HEATMAP ACROSS ALL SAMPLES    ##
+################################################
+################################################
+
+if (ncol(dat) == 6 && length(INPUT_FILES) > 1) {
+    mat <- spread(dat[,c("sample", "region", "coverage")], sample, coverage, fill=NA, convert=FALSE)
+    rownames(mat) <- mat[,1]
+    mat <- as.matrix(log10(mat[,-1] + 1))
+    heatmap <- Heatmap(mat,
+                       name                = "log10(Coverage+1)",
+                       cluster_rows        = FALSE,
+                       cluster_columns     = FALSE,
+                       show_row_names      = TRUE,
+                       show_column_names   = TRUE,
+                       column_names_side   = "bottom",
+                       rect_gp             = gpar(col="white", lwd=1),
+                       show_heatmap_legend = TRUE,
+                       row_names_gp        = gpar(fontsize=6),
+                       column_names_gp     = gpar(fontsize=6),
+                       height              = unit(5, "mm")*nrow(mat),
+                       width               = unit(5, "mm")*ncol(mat),
+                       col                 = viridis(50))
+
+    ## Size of heatmaps scaled based on matrix dimensions: https://jokergoo.github.io/ComplexHeatmap-reference/book/other-tricks.html#set-the-same-cell-size-for-different-heatmaps-with-different-dimensions
+    width = 0.1969*ncol(mat) + (2*1.3150)
+    height = 0.1969*nrow(mat) + 1.3150
+    outfile <- paste(OUTDIR,"all_samples.",opt$output_suffix,".heatmap.pdf", sep='')
+    pdf(file=outfile, width=width, height=height)
+    draw(heatmap)
+    dev.off()
+}
 
 ################################################
 ################################################
