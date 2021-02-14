fe12a6124d3be3d85898ee7f9ddc053bf77c0138
drpatelh <drhpatel@gmail.com>
2021-02-14 14:31:50 +0000

    Add separate function to validate ONT samplesheet

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# This script is based on the example at: https://github.com/nf-core/test-datasets/blob/viralrecon/samplesheet/samplesheet_test_amplicon.csv
-
 import os
 import sys
 import errno
@@ -14,6 +12,7 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument("FILE_IN", help="Input samplesheet file.")
     parser.add_argument("FILE_OUT", help="Output file.")
+    parser.add_argument('-pl', '--platform', type=str, dest="PLATFORM", default='illumina', help="Sequencing platform for input data. Accepted values = 'illumina' or 'nanopore'  (default: 'illumina').")
     return parser.parse_args(args)
 
 def make_dir(path):
@@ -33,7 +32,7 @@ def print_error(error, context="Line", context_str=""):
     print(error_str)
     sys.exit(1)
 
-def check_samplesheet(file_in, file_out):
+def check_illumina_samplesheet(file_in, file_out):
     """
     This function checks that the samplesheet follows the following structure:
 
@@ -41,9 +40,12 @@ def check_samplesheet(file_in, file_out):
     SAMPLE_PE,SAMPLE_PE_RUN1_1.fastq.gz,SAMPLE_PE_RUN1_2.fastq.gz
     SAMPLE_PE,SAMPLE_PE_RUN2_1.fastq.gz,SAMPLE_PE_RUN2_2.fastq.gz
     SAMPLE_SE,SAMPLE_SE_RUN1_1.fastq.gz,
+
+    For an example see:
+    https://github.com/nf-core/test-datasets/blob/viralrecon/samplesheet/samplesheet_test_amplicon.csv
     """
 
-    sample_run_dict = {}
+    sample_mapping_dict = {}
     with open(file_in, "r") as fin:
 
         ## Check header
@@ -103,34 +105,119 @@ def check_samplesheet(file_in, file_out):
                 print_error("Invalid combination of columns provided!", "Line", line)
 
             ## Create sample mapping dictionary = { sample: [ single_end, fastq_1, fastq_2 ] }
-            if sample not in sample_run_dict:
-                sample_run_dict[sample] = [sample_info]
+            if sample not in sample_mapping_dict:
+                sample_mapping_dict[sample] = [sample_info]
             else:
-                if sample_info in sample_run_dict[sample]:
+                if sample_info in sample_mapping_dict[sample]:
                     print_error("Samplesheet contains duplicate rows!", "Line", line)
                 else:
-                    sample_run_dict[sample].append(sample_info)
+                    sample_mapping_dict[sample].append(sample_info)
 
     ## Write validated samplesheet with appropriate columns
-    if len(sample_run_dict) > 0:
+    if len(sample_mapping_dict) > 0:
         out_dir = os.path.dirname(file_out)
         make_dir(out_dir)
         with open(file_out, "w") as fout:
             fout.write(",".join(["sample", "single_end", "fastq_1", "fastq_2"]) + "\n")
-            for sample in sorted(sample_run_dict.keys()):
+            for sample in sorted(sample_mapping_dict.keys()):
 
                 ## Check that multiple runs of the same sample are of the same datatype
-                if not all(x[0] == sample_run_dict[sample][0][0] for x in sample_run_dict[sample]):
+                if not all(x[0] == sample_mapping_dict[sample][0][0] for x in sample_mapping_dict[sample]):
                     print_error("Multiple runs of a sample must be of the same datatype!","Sample: {}".format(sample))
 
-                for idx,val in enumerate(sample_run_dict[sample]):
+                for idx,val in enumerate(sample_mapping_dict[sample]):
                     fout.write(','.join(["{}_T{}".format(sample,idx+1)] + val) + '\n')
+    else:
+        print_error("No entries to process!","Samplesheet: {}".format(file_in))
+
+
+def check_nanopore_samplesheet(file_in, file_out):
+    """
+    This function checks that the samplesheet follows the following structure:
+
+    sample,barcode
+    SAMPLE_N,1
+    SAMPLE_X,2
+    SAMPLE_Z,3
+
+    For an example see:
+    https://github.com/nf-core/test-datasets/blob/viralrecon/samplesheet/samplesheet_test_nanopore.csv
+    """
+
+    sample_mapping_dict = {}
+    with open(file_in, "r") as fin:
+
+        ## Check header
+        MIN_COLS = 2
+        HEADER = ['sample', 'barcode']
+        header = [x.strip('"') for x in fin.readline().strip().split(",")]
+        if header[: len(HEADER)] != HEADER:
+            print("ERROR: Please check samplesheet header -> {} != {}".format(",".join(header), ",".join(HEADER)))
+            sys.exit(1)
+
+        ## Check sample entries
+        for line in fin:
+            lspl = [x.strip().strip('"') for x in line.strip().split(",")]
+
+            # Check valid number of columns per row
+            if len(lspl) < len(HEADER):
+                print_error(
+                    "Invalid number of columns (minimum = {})!".format(len(HEADER)),
+                    "Line",
+                    line,
+                )
+            num_cols = len([x for x in lspl if x])
+            if num_cols < MIN_COLS:
+                print_error(
+                    "Invalid number of populated columns (minimum = {})!".format(MIN_COLS),
+                    "Line",
+                    line,
+                )
+
+            ## Check sample entry
+            sample, barcode = lspl[: len(HEADER)]
+            if sample:
+                if sample.find(" ") != -1:
+                    print_error("Sample entry contains spaces!", "Line", line)
+            else:
+                print_error("Sample entry has not been specified!", "Line", line)
+
+            ## Check barcode entry
+            if barcode:
+                if not barcode.isdigit():
+                    print_error("Barcode entry is not an integer!", 'Line', line)
+                else:
+                    barcode = 'barcode%s' % (barcode.zfill(2))
+                
+            ## Create sample mapping dictionary = { sample: barcode }
+            if barcode in sample_mapping_dict.values():
+                print_error("Samplesheet contains duplicate entries in the 'barcode' column!", "Line", line)
+            if sample not in sample_mapping_dict:
+                sample_mapping_dict[sample] = barcode
+            else:
+                print_error("Samplesheet contains duplicate entries in the 'sample' column!", "Line", line)
+
+    ## Write validated samplesheet with appropriate columns
+    if len(sample_mapping_dict) > 0:
+        out_dir = os.path.dirname(file_out)
+        make_dir(out_dir)
+        with open(file_out, "w") as fout:
+            fout.write(",".join(["sample", "barcode"]) + "\n")
+            for sample in sorted(sample_mapping_dict.keys()):
+                fout.write(','.join([sample, sample_mapping_dict[sample]]) + '\n')
     else:
         print_error("No entries to process!","Samplesheet: {}".format(file_in))
 
 def main(args=None):
     args = parse_args(args)
-    check_samplesheet(args.FILE_IN, args.FILE_OUT)
+
+    if args.PLATFORM == 'illumina':
+        check_illumina_samplesheet(args.FILE_IN, args.FILE_OUT)
+    elif args.PLATFORM == 'nanopore':
+        check_nanopore_samplesheet(args.FILE_IN, args.FILE_OUT)
+    else:
+        print("Unrecognised option passed to --platform: {}. Accepted values = 'illumina' or 'nanopore'".format(args.PLATFORM))
+        sys.exit(1)
 
 if __name__ == "__main__":
     sys.exit(main())
