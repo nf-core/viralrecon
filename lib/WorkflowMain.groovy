2db0c9e17cad7ff1051e0bf661f7b6321b4cd343
Harshil Patel <drpatelhh@gmail.com>
2022-01-17 17:50:01 +0000

    Add --nextclade_dataset param

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -80,6 +80,14 @@ class WorkflowMain {
             log.error "Invalid platform option: '${params.platform}'. Valid options: ${platformList.join(', ')}."
             System.exit(1)
         }
+
+        // Check Nextclade dataset parameters
+        if (!params.skip_nextclade) {
+            if (!params.nextclade_dataset && !params.nextclade_dataset_name) {
+                log.error "Nextclade dataset not specified with '--nextclade_dataset' or '--nextclade_dataset_name'. A list of available datasets can be obtained using the Nextclade 'nextclade dataset list' command."
+                System.exit(1)
+            }
+        }
     }
 
     //
